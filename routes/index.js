f48de49321df4033b16f9103b5069a0b93ca8b77
Jessie Texier <jessitexier8@gmail.com>
2025-12-02 16:34:24 +0100

    detail catway, bootstrap icons, modif catway

--- routes/index.js
+++ routes/index.js
@@ -30,19 +30,62 @@ router.post('/login', async (req, res) => {
     }
 });
 
-// Route pour le tableau de bord
+// Dashboard route
 router.get('/dashboard', (req, res) => {
-    res.render('dashboard');
+    res.render('dashboard', { title: 'Tableau de bord - Russell' });
 });
 
 
-// Page : Liste des catways
+// Liste catways
 router.get('/catways-page', async (req, res) => {
     try {
         const catways = await catwayService.getAllCatways();
-        res.render('catways', { catways: catways });
+        res.render('catways', {
+            catways: catways,
+            title: 'Liste des Catways'
+        });
     } catch (error) {
         res.status(500).send("Erreur lors de la récupération des catways");
+    }
+});
+
+// détail catway
+// 1. AFFICHER (GET)
+// On change '/catways/:id' en '/catway-detail/:id'
+router.get('/catway-detail/:id', async (req, res) => {
+    try {
+        const catway = await catwayService.getCatwayByNumber(req.params.id);
+        if (!catway) return res.status(404).send("Introuvable");
+        
+        res.render('catway-detail', { 
+            catway: catway,
+            title: `Catway n°${catway.catwayNumber} - Détails` 
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Erreur serveur");
+    }
+});
+
+// 2. MODIFIER (POST)
+router.post('/catway-detail/:id', async (req, res) => {
+    try {
+        await catwayService.updateCatway(req.params.id, req.body);
+        res.redirect('/catways-page'); 
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Erreur lors de la mise à jour");
+    }
+});
+
+// 3. SUPPRIMER (POST)
+router.post('/catway-detail/:id/delete', async (req, res) => {
+    try {
+        await catwayService.deleteCatway(req.params.id);
+        res.redirect('/catways-page'); 
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Erreur lors de la suppression");
     }
 });
 
